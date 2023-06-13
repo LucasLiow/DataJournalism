6cd89948bce2fe9208becd47c157d080073a1498
LucasLiow <lucas.liow24@trinityschoolnyc.org>
2023-06-13 12:03:08 -0400

    final

--- server.py
+++ server.py
@@ -8,70 +8,49 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
-    f =open("data/life_expectancy.json","r")
+    f =open("data/state_crime.json","r")
     data = json.load(f)
     f.close()
-
-    total = 0
-
-    can = data["Canada"]
-    yearsC = sorted(list(can.keys()))
-
-    for num in can:
-       total+= can[num]
-
-    line_endpointsC =[]
-    for i in range(len(yearsC)-1): # make it easy to dynamically generate a line graph
-        start_x = yearsC[i] #generate endpoints for each line segment
-        stop_x = yearsC[i+1]
-        line_endpointsC.append([can[start_x],can[stop_x]] )
-
-    mex = data["Mexico"]
-    yearsM = sorted(list(mex.keys()))
-
-    for num2 in mex:
-        total += mex[num2]
-
-    line_endpointsM =[]
-    for i in range(len(yearsM)-1): # make it easy to dynamically generate a line graph
-        start_x = yearsM[i] #generate endpoints for each line segment
-        stop_x = yearsM[i+1]
-        line_endpointsM.append([mex[start_x],mex[stop_x]] )
-
-    usa = data["United States"]
-    yearsA = sorted(list(usa.keys()))
-
-    for num3 in usa:
-        total+=usa[num3]
-
-    avg = total/(len(usa)*3)
-
-    line_endpointsA =[]
-    for i in range(len(yearsA)-1): # make it easy to dynamically generate a line graph
-        start_x = yearsA[i] #generate endpoints for each line segment
-        stop_x = yearsA[i+1]
-        line_endpointsA.append([usa[start_x],usa[stop_x]] )
-
-    return render_template('index.html', yearsC = yearsC, endpointsC = line_endpointsC,  yearsM = yearsM, endpointsM = line_endpointsM, yearsA = yearsA, endpointsA = line_endpointsA, avg = avg)
+    Alabama = data["Alabama"]
+    years = sorted(list(Alabama.keys()))
+    return render_template('index.html',years = years, data = data)
   
 
-@app.route('/year')
-def year():
-
-    f =open("data/life_expectancy.json","r")
+@app.route('/macro')
+def macro():
+    f =open("data/state_crime.json","r")
     data = json.load(f)
     f.close()
+    numbers={}
+    for state in data.keys():
+        numbers[state]=data[state]["2019"]
 
-    can = data["Canada"]
-    yearsC = sorted(list(can.keys()))
+    return render_template('macro.html', numbers = numbers)
 
-    mex = data["Mexico"]
-    yearsM = sorted(list(mex.keys()))
+@app.route('/micro')
+def micro():
+    f =open("data/state_crime.json","r")
+    data = json.load(f)
+    f.close()
+    state = request.args.get('state')
 
-    usa = data["United States"]
-    yearsA = sorted(list(usa.keys()))
+    stateData = data[state]
+    years = sorted(list(stateData.keys()))
+    endpoints = []
+    for i in range(len(years)-1):
+        start_x = years[i] #generate endpoints for each line segment
+        stop_x = years[i+1]
+        endpoints.append([data[state][start_x],data[state][stop_x]])
 
-    year = request.args.get('year')
-    return render_template('year.html', year = year, can = can, mex = mex, usa = usa)
+    total = 0
+    for date in data[state]:
+        total += data[state][date]
 
+    avg = total/len(data[state])
+    gl = ""
+    if avg >= 45950:
+        gl = "higher"
+    else:
+        gl = "lower"
+    return render_template('micro.html', state = state, endpoints = endpoints, avg = avg, gl = gl)
 app.run(debug=True)
